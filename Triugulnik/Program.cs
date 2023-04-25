21d1aa5f06a084ac21b2f64e05b67876b12771a5
DiyanTrifonov <diyantrifonov5@gmail.com>
2023-04-25 15:11:52 +0300

    First commit

---
+++ Triugulnik/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triugulnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи страната А: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Въведи височината H: ");
+            double h = double.Parse(Console.ReadLine());
+
+            Triangle triangle = new Triangle(a, h);
+            Console.WriteLine($"Триъгълник със страна {triangle.PrintA()}cm и височина към нея {triangle.PrintH()}cm.");
+
+            triangle.Print();
+        }
+    }
+}
---
+++ Triugulnik/Triangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triugulnik
+{
+    internal class Triangle
+    {
+        private double a;
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        private double h;
+
+        public double H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public Triangle(double a, double h)
+        {
+            this.A = a;
+            this.H = h;
+        }
+
+        public double Area()
+        {
+            double area;
+            area = a * h / 2;
+            return area;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Лицето = {a} * {h} / 2 = {Area()} cm2!");
+        }
+
+        public double PrintA()
+        {
+            return a;
+        }
+
+        public double PrintH()
+        {
+            return h;
+        }
+    }
+}
